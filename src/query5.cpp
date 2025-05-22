17901a6dd38cbd3823202c8c2a391049c667bcd6
yogendra123456 <48909494+yogendra123456@users.noreply.github.com>
2025-05-22 14:01:13 +0530

    Update query5.cpp

--- src/query5.cpp
+++ src/query5.cpp
@@ -9,26 +9,18 @@
 std::mutex result_mutex;
 // Function to parse command line arguments
 bool parseArgs(int argc, char* argv[], std::string& r_name, std::string& start_date, std::string& end_date, int& num_threads, std::string& table_path, std::string& result_path) {
-    if (argc != 13)
-        return false;
+    if (argc != 13) return false;
 
     for (int i = 1; i < argc; i += 2) {
         std::string key = argv[i];
         std::string val = argv[i + 1];
-        if (key == "--r_name") 
-            r_name = val;
-        else if (key == "--start_date") 
-            start_date = val;
-        else if (key == "--end_date") 
-            end_date = val;
-        else if (key == "--threads") 
-            num_threads = std::stoi(val);
-        else if (key == "--table_path") 
-            table_path = val;
-        else if (key == "--result_path") 
-            result_path = val;
-        else 
-            return false;
+        if (key == "--r_name") r_name = val;
+        else if (key == "--start_date") start_date = val;
+        else if (key == "--end_date") end_date = val;
+        else if (key == "--threads") num_threads = std::stoi(val);
+        else if (key == "--table_path") table_path = val;
+        else if (key == "--result_path") result_path = val;
+        else return false;
     }
 
     return true;
@@ -37,13 +29,14 @@ bool parseArgs(int argc, char* argv[], std::string& r_name, std::string& start_d
 
 // Function to read TPCH data from the specified paths
 bool readTPCHData(const std::string& table_path, std::vector<std::map<std::string, std::string>>& customer_data, std::vector<std::map<std::string, std::string>>& orders_data, std::vector<std::map<std::string, std::string>>& lineitem_data, std::vector<std::map<std::string, std::string>>& supplier_data, std::vector<std::map<std::string, std::string>>& nation_data, std::vector<std::map<std::string, std::string>>& region_data) {
-    return readCSV(path + "/customer.tbl", customer_data) &&
-           readCSV(path + "/orders.tbl", orders_data) &&
-           readCSV(path + "/lineitem.tbl", lineitem_data) &&
-           readCSV(path + "/supplier.tbl", supplier_data) &&
-           readCSV(path + "/nation.tbl", nation_data) &&
-           readCSV(path + "/region.tbl", region_data);
+    return readCSV(table_path + "/customer.tbl", customer_data) &&
+           readCSV(table_path + "/orders.tbl", orders_data) &&
+           readCSV(table_path + "/lineitem.tbl", lineitem_data) &&
+           readCSV(table_path + "/supplier.tbl", supplier_data) &&
+           readCSV(table_path + "/nation.tbl", nation_data) &&
+           readCSV(table_path + "/region.tbl", region_data);
 }
+
 bool readCSV(const std::string& filename, std::vector<std::map<std::string, std::string>>& table) {
     std::ifstream file(filename);
     if (!file.is_open()) return false;
@@ -53,8 +46,7 @@ bool readCSV(const std::string& filename, std::vector<std::map<std::string, std:
     if (std::getline(file, line)) {
         std::stringstream ss(line);
         std::string cell;
-        while (std::getline(ss, cell, '|')) 
-                headers.push_back(cell);
+        while (std::getline(ss, cell, '|')) headers.push_back(cell);
     }
 
     while (std::getline(file, line)) {
@@ -99,9 +91,8 @@ bool executeQuery5(const std::string& r_name, const std::string& start_date, con
 
 // Function to output results to the specified path
 bool outputResults(const std::string& result_path, const std::map<std::string, double>& results) {
-   std::ofstream out(result_path + "/query5_result.txt");
-    if (!out.is_open()) 
-    return false;
+    std::ofstream out(result_path + "/query5_result.txt");
+    if (!out.is_open()) return false;
 
     for (const auto& [nation, revenue] : results) {
         out << nation << "|" << revenue << "\n";
